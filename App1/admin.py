a2423013d07508b00aceaa2e3e5417fd2093d09c
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2025-11-26 01:47:02 +0600

    update python verson and  create a regester base Model section and show admin panal all regester users.
    use of query

--- App1/admin.py
+++ App1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Student_Registration
 # Register your models here.
+
+
+@admin.register(Student_Registration)
+class Student_Registration_Admin(admin.ModelAdmin):
+    list_display = ('stu_id', 'name', 'email', 'phone_number', 'designation', 'department', 'is_active')
+    search_fields = ('stu_id', 'name', 'email', 'department')
+    list_filter = ('is_active', 'department', 'designation')
+    ordering = ('stu_id',)
--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Student_Registration(models.Model):
+    stu_id = models.CharField(max_length=30, unique=True, help_text="Unique Id Assigned to Each Student")
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150,unique=True)
+    phone_number = models.CharField(max_length=15)
+    designation = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='student_images/')
+    is_active = models.BooleanField(default=True)
+    # registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.stu_id})"
