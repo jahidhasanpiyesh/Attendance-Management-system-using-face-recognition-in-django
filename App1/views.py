81d8dae429a6cedc721203f220a1995588dadf12
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2025-11-26 03:34:42 +0600

    Update and fixed student regester process and proper work data base. and save django admin panal this data.

--- App1/views.py
+++ App1/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect, render
 from .models import Student_Registration
+from django.contrib import messages
+from django.core.files.base import ContentFile
+import base64
 # Create your views here.
 
 # ----------------------------------------View for rendering the home page---------------------------
@@ -9,15 +12,25 @@ def home(request):
 # --------------------------------------View for rendering the student registration page---------------------------
 def register_stu(request):
     if request.method == 'POST':
-        stu_id = request.POST.get('stu_id')
         name = request.POST.get('name')
+        stu_id = request.POST.get('stu_id')
         email = request.POST.get('email')
         phone_number = request.POST.get('phone_number')
         designation = request.POST.get('designation')
         department = request.POST.get('department')
-
-        profile_image = request.FILES.get('profile_image')
         image_data = request.POST.get('image_data')
+
+        # Decode the base64 image data
+        profile_image = None
+        if image_data:
+            try:
+                header, encoded = image_data.split(',', 1)
+                profile_image = ContentFile(base64.b64decode(encoded), name=f"{stu_id}.jpg")
+            except Exception as e:
+                messages.error(request, "Error processing profile image. please try again!")
+                print(f"Error decoding image: {e}")
+                return render(request, 'register_stu.html')
+
 
         student_registration = Student_Registration(
             stu_id=stu_id,
@@ -27,12 +40,19 @@ def register_stu(request):
             designation=designation,
             department=department,
             profile_image=profile_image,
-            image_data=image_data,
             is_active = True
         )
-        student_registration.save()
-        massage = "Student Registered Successfully"
-        return redirect('register_success')
+        
+        # Save the student registration data to the database
+        try:
+            student_registration.save()
+            messages.success(request, "Student Registered Successfully")
+            return redirect('register_success')
+        except Exception as e:
+            messages.error(request, "Error saving student registration. Please try again.")
+            print(f"Error saving student registration: {e}")
+            return render(request, 'register_stu.html')
+        
     return render(request, 'register_stu.html')
 
 # ----------------------------------------View for rendering the registration success page---------------------------
