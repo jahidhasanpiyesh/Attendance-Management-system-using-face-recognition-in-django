a0dc89e7d8a19e489c425e7d12781dddec418744
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2025-12-05 00:52:14 +0600

    added admin panal cameraconfigurations data .

--- App1/admin.py
+++ App1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student_Registration
+from .models import Student_Registration, CameraConfiguration
 # Register your models here.
 
 
@@ -9,3 +9,9 @@ class Student_Registration_Admin(admin.ModelAdmin):
     search_fields = ('stu_id', 'name', 'email', 'department')
     list_filter = ('is_active', 'department', 'designation')
     ordering = ('stu_id',)
+
+
+@admin.register(CameraConfiguration)
+class CameraConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'camera_source', 'threshold']
+    search_fields = ['name']
