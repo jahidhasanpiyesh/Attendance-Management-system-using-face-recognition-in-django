17632897bcc36b579a418680d3e871418ea3e7d6
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2025-12-09 00:44:09 +0600

    Update forms.py section and comment set all update.

--- App1/forms.py
+++ App1/forms.py
@@ -1,11 +1,14 @@
 # forms.py
 from django import forms
+
+# import the model for which the form is being created
 from .models import UploadedImage
 
+# form for uploading an image
 class UploadImageForm(forms.ModelForm):
     name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     image = forms.ImageField(label='Image', widget=forms.FileInput(attrs={'class': 'form-control-file'}))
 
     class Meta:
-        model = UploadedImage
+        model = UploadedImage # specify the model to be used
         fields = ['name', 'image']
